b996032e0336d54d4b5bd3501519e4ae10f73b52
the_Squirtle321 <wilcorhy001@warren.k12.in.us>
2025-03-05 11:58:29 -0500

    Update maine.py
    vegetation consisting of typically short plants with long, narrow leaves, growing wild or cultivated on lawns and pasture, and as a fodder crop.

--- maine.py
+++ maine.py
@@ -1,11 +1,19 @@
-class Book:
-  def __init__(self, title, author):
-    self.title = title
-    self.author = author
+class BankAccount:
+  bank_name = "Fred's Bank"
+  num_account = 0
+  BankAccount.num_account = num_account + 1
 
-class Library:
-  def __init__(self):
-    self.books = []
+  def __init__(self, account_holder, balance):
+    self.account_holder = account_holder
+    self.balance = balance
 
-  def add_book(self, book):
-    
+  def withdraw(self, amount):
+    self.balance = self.balance - 1
+
+  @classmethod
+  def display_bank_info(cls):
+    print(cls, num_account)
+
+account_1 = BankAccount("Charlie", 100)
+account_2 = BankAccount("Mike", 200)
+BankAccount.display_bank_info()
